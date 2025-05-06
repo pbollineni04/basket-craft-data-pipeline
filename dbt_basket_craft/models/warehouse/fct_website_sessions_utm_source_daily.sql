671974a290fe1ba6912e7be1dc6438e47c48ac75
pbollineni04 <68767782+pbollineni04@users.noreply.github.com>
2025-05-06 17:16:38 +0000

    Added utm source to fct_website_sessions_utm_source_daily model

--- dbt_basket_craft/models/warehouse/fct_website_sessions_utm_source_daily.sql
+++ dbt_basket_craft/models/warehouse/fct_website_sessions_utm_source_daily.sql
@@ -1,11 +1,12 @@
 WITH fct_website_sessions_utm_source_daily AS (
    SELECT
+       utm_source,
        DATE(website_session_created_at) AS website_session_day,
        COUNT(website_session_id) AS sessions,
        SUM(is_repeat_session)::int AS repeat_sessions,
        (SUM(is_repeat_session)::numeric / COUNT(website_session_id)) * 100 AS repeat_sessions_pct,
        CURRENT_TIMESTAMP AS loaded_at
-   FROM {{ ref('stg_website_sessions') }} o
+   FROM {{ ref('stg_website_sessions') }}
    GROUP BY 
         DATE(website_session_created_at), 
         utm_source
